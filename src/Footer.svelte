dca0dfad2fe110b454c859c2b99ede0c17a8ea7a
Michael Garten <mjgartendev@gmail.com>
2019-06-12 23:18:12 -0500

    fixed github link

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,7 +1,7 @@
 <footer id="btm" class="bg-grey fg-dark flex-center">
   <slot>
-    &copy; 2019  Michael Garten 
-    <a href="https://www.github.com/mjgartendev/template" target="_blank" rel="noreferrer">
+    &copy; 2019  mjgartendev 
+    <a href="https://www.github.com/mjgartendev/svelte-app" target="_blank" rel="noreferrer">
       <span class="fg-dark fab fa-github"></span>
     </a>
   </slot> 
